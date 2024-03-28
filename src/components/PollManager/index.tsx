cf8892f3f4bda454e2f2d5eb669233b1ab56012f
Hob Adams <hob.adams@motivepartners.com>
2024-03-28 20:11:03 +0000

    start work

--- src/components/PollManager/index.tsx
+++ src/components/PollManager/index.tsx
@@ -4,11 +4,15 @@ import Results from '../Results';
 import { pollData, Poll } from '../../types/Poll';
 
 const PollManager: React.FC = () => {
+  const [showWinner, setShowWinner] = useState<boolean>(false);
+  const { options, question } = pollData;
+  //const [, setPollData] = useState<Poll>(initialPollData);
+
   return (
     <div className="layout-column align-items-center justify-content-start poll-manager" data-testid="poll-manager">
-      <h2>Add Question Here</h2>
-      <Vote options={pollData.options} onVote={() => {}} viewWinner={false} />
-      <Results poll={pollData} viewWinner={false} setViewWinner={() => {}} totalVotes={0} />
+      <h2>{question}</h2>
+      <Vote options={pollData.options} onVote={() => { }} viewWinner={false} />
+      <Results poll={pollData} viewWinner={showWinner} setViewWinner={() => setShowWinner(true)} totalVotes={0} />
     </div>
   );
 };
--- src/components/Vote/index.tsx
+++ src/components/Vote/index.tsx
@@ -10,28 +10,30 @@ interface VoteProps {
 const Vote: React.FC<VoteProps> = ({ options, onVote, viewWinner }) => {
   return (
     <div className="vote-container w-40 h-50">
-      <div key={1} data-testid={`option-1`} className="card mr-10 ml-10 w-100">
-        <section className="layout-row align-items-center justify-content-center mr-10 ml-10 pr-10 pl-10">
-          <h3 data-testid={`choice-1}`}>{"Choice 1"}</h3>
-        </section>
-        <section className="layout-row align-items-center justify-content-center mr-10 ml-10 mb-10 pr-10 pl-10">
-          <button onClick={() => {}} disabled={false} data-testid={`vote-button-1`}>
-            Vote
-          </button>
-        </section>
-      </div>
-      <div key={2} data-testid={`option-2`} className="card mr-10 ml-10 w-100">
-        <section className="layout-row align-items-center justify-content-center mr-10 ml-10 pr-10 pl-10">
-          <h3 data-testid={`choice-2`}>{"Choice 2"}</h3>
-        </section>
-        <section className="layout-row align-items-center justify-content-center mr-10 ml-10 mb-10 pr-10 pl-10">
-          <button onClick={() => {}} disabled={false}data-testid={`vote-button-2`} >
-            Vote
-          </button>
-        </section>
-      </div>
+      {options.map((option) => (
+        <VoteButton key={option.id} text={option.text} onClick={() => onVote(option.id)} disabled={viewWinner} />
+      ))}
     </div>
   );
 };
+
+interface VoteButtonProps {
+  text: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const VoteButton = ({ text, onClick, disabled = false }: VoteButtonProps) => (
+  <div key={1} data-testid={`option-1`} className="card mr-10 ml-10 w-100">
+    <section className="layout-row align-items-center justify-content-center mr-10 ml-10 pr-10 pl-10">
+      <h3 data-testid={`choice-1}`}>{text}</h3>
+    </section>
+    <section className="layout-row align-items-center justify-content-center mr-10 ml-10 mb-10 pr-10 pl-10">
+      <button onClick={onClick} disabled={disabled} data-testid={`vote-button-1`}>
+        Vote
+      </button>
+    </section>
+  </div>
+)
 
 export default Vote;
