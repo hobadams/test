4b57511022ffb8c5502c4ef0d21a062f24383fb3
Hob Adams <hob.adams@motivepartners.com>
2024-03-28 20:30:38 +0000

    add logic

--- src/components/PollManager/index.tsx
+++ src/components/PollManager/index.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import Vote from '../Vote';
 import Results from '../Results';
-import { pollData, Poll } from '../../types/Poll';
+import { pollData as initialPollData, Poll } from '../../types/Poll';
 
 const PollManager: React.FC = () => {
   const [showWinner, setShowWinner] = useState<boolean>(false);
-  const { options, question } = pollData;
-  //const [, setPollData] = useState<Poll>(initialPollData);
+  const [pollData, setPollData] = useState<Poll>(initialPollData);
+
+  const vote = (id: number) => {
+    const updatedPollData = { ...pollData };
+    const option = updatedPollData.options.find((option) => option.id === id);
+    if (option) {
+      option.votes += 1;
+      setPollData(updatedPollData);
+    }
+  }
 
   return (
     <div className="layout-column align-items-center justify-content-start poll-manager" data-testid="poll-manager">
-      <h2>{question}</h2>
-      <Vote options={pollData.options} onVote={() => { }} viewWinner={false} />
-      <Results poll={pollData} viewWinner={showWinner} setViewWinner={() => setShowWinner(true)} totalVotes={0} />
+      <h2>{pollData.question}</h2>
+      <Vote options={pollData.options} onVote={vote} viewWinner={false} />
+      <Results poll={pollData} viewWinner={showWinner} setViewWinner={setShowWinner} totalVotes={0} />
     </div>
   );
 };
--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Poll } from '../../types/Poll';
+import { Poll, PollOption } from '../../types/Poll';
 
 interface ResultsProps {
   poll: Poll;
@@ -9,16 +9,59 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = ({ poll, viewWinner, setViewWinner, totalVotes }) => {
+  const hasVotes = poll.options.some((option) => option.votes > 0);
   return (
     <>
-      <p data-testid="result">{"Add Result Here"}</p>
+      <p data-testid="result">{getResultsText(poll, viewWinner)}</p>
       <section className="layout-row align-items-center justify-content-center mr-10 ml-10 pr-10 pl-10">
-        <button data-testid="winner-button" onClick={() => {}} disabled={false}>
+        <button data-testid="winner-button" onClick={() => setViewWinner(!viewWinner)} disabled={!hasVotes || viewWinner}>
           View Winner
         </button>
       </section>
     </>
   );
 };
+
+const getResultsText = (poll: Poll, viewWinner: boolean): string => {
+
+  const hasVotes = poll.options.some((option) => option.votes > 0);
+
+  if (!hasVotes) return 'No votes yet!';
+
+  const voteDifference = getMostVotedWithDifference(poll.options);
+
+  if (voteDifference === null) return 'Its a tie!';
+
+  if (viewWinner) {
+    return `${poll.options.find((option) => option.id === voteDifference.id)?.text} won by ${voteDifference.difference} vote(s)`;
+  }
+
+  return `${poll.options.find((option) => option.id === voteDifference.id)?.text} is winning with a difference of ${voteDifference.difference} vote(s)`;
+
+}
+
+interface OptionWIthVoteDifferent {
+  id: number,
+  difference: number
+}
+
+const getMostVotedWithDifference = (options: PollOption[]): OptionWIthVoteDifferent | null => {
+
+  if (options.length < 2) return null;
+
+  const sortedOptions = [...options].sort((a, b) => b.votes - a.votes);
+
+  const mostVoted = sortedOptions[0];
+  const secondMostVoted = sortedOptions[1];
+
+  const voteDifference = mostVoted.votes - secondMostVoted.votes;
+
+  if (voteDifference === 0) return null;
+
+  return {
+    id: mostVoted.id,
+    difference: voteDifference,
+  };
+}
 
 export default Results;
